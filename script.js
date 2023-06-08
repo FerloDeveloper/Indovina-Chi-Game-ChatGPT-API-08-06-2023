0f520f07294095b343578c1e2d916682c648faf3
FerloDeveloper <gianluca.laferla.dev@gmail.com>
2023-06-08 22:14:23 +0200

    img fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const MODEL = "gpt-3.5-turbo";
-const API_KEY = "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";
+const API_KEY = "";
 const input = document.querySelector(".input");
 let inputValue = input.value;
 const messageArea = document.querySelector(`.message-area`);
